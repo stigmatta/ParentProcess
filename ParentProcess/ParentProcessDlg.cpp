301b3fdc765aeee3bed2a05027dfeb29d82e5e4e
Andrey Odintsov <morrigun0@gmail.com>
2024-02-27 10:42:49 +0200

    Add project files.

---
+++ ParentProcess/ParentProcessDlg.cpp
@@ -0,0 +1,94 @@
+#include "ParentProcessDlg.h"
+
+ParentProcessDlg* ParentProcessDlg::ptr = NULL;
+
+ParentProcessDlg::ParentProcessDlg(void)
+{
+	ptr = this;
+}
+
+ParentProcessDlg::~ParentProcessDlg(void)
+{
+	
+}
+
+void ParentProcessDlg::Cls_OnClose(HWND hwnd)
+{
+	EndDialog(hwnd, 0);
+}
+
+void MessageAboutError(DWORD dwError)
+{
+	LPVOID lpMsgBuf = NULL;
+	TCHAR szBuf[300]; 
+
+	BOOL fOK = FormatMessage(
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+		NULL, dwError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf, 0, NULL);
+	if(lpMsgBuf != NULL)
+	{
+		wsprintf(szBuf, TEXT("Ошибка %d: %s"), dwError, lpMsgBuf); 
+		MessageBox(0, szBuf, TEXT("Сообщение об ошибке"), MB_OK | MB_ICONSTOP); 
+		LocalFree(lpMsgBuf);
+	}
+}
+
+void ParentProcessDlg::Cls_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+	if(IDC_BUTTON1 == id)
+	{
+		CreateMutex(0, FALSE, TEXT("{B8A2C367-10FE-494d-A869-841B2AF972E0}"));
+		hMutex = OpenMutex(MUTEX_ALL_ACCESS, false, TEXT("{B8A2C367-10FE-494d-A869-841B2AF972E0}"));
+		DWORD dwAnswer = WaitForSingleObject(hMutex, INFINITE);
+
+		STARTUPINFO st = {sizeof(st)};
+		PROCESS_INFORMATION pr;
+		TCHAR filename[20];
+		wsprintf(filename, TEXT("%s"), TEXT("CodingThread.exe"));
+		
+		if (!CreateProcess(NULL, filename, NULL, NULL, 0, 0, NULL, NULL, &st, &pr))
+		{
+			MessageAboutError(GetLastError());
+			return;
+		}
+		CloseHandle(pr.hThread);
+		CloseHandle(pr.hProcess);
+
+		ReleaseMutex(hMutex);
+
+		CreateMutex(0, FALSE, TEXT("{B8A2C367-10FE-494d-A869-841B2AF972E0}"));
+		hMutex = OpenMutex(MUTEX_ALL_ACCESS, false, TEXT("{B8A2C367-10FE-494d-A869-841B2AF972E0}"));
+		dwAnswer = WaitForSingleObject(hMutex, INFINITE);
+
+		STARTUPINFO st2 = { sizeof(st2) };
+		PROCESS_INFORMATION pr2;
+		TCHAR filename2[20];
+		wsprintf(filename2, TEXT("%s"), TEXT("CodingThread2.exe"));
+
+		if (!CreateProcess(NULL, filename2, NULL, NULL, 0, 0, NULL, NULL, &st2, &pr2))
+		{
+			MessageAboutError(GetLastError());
+			return;
+		}
+		CloseHandle(pr2.hThread);
+		CloseHandle(pr2.hProcess);
+
+		ReleaseMutex(hMutex);
+
+		//
+		/// Запуск второго процесса! вывод текста на екран!
+		////
+		
+	}
+}
+
+BOOL CALLBACK ParentProcessDlg::DlgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch(message)
+	{
+		HANDLE_MSG(hwnd, WM_CLOSE, ptr->Cls_OnClose);
+		HANDLE_MSG(hwnd, WM_COMMAND, ptr->Cls_OnCommand);
+	}
+	return FALSE;
+}
